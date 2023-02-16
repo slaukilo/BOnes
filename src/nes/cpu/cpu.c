1600968b34ff8f205b596f16a028d5da1c2ee4ab
Sarah Lau-Kilo <slaukilo@gmail.com>
2023-02-16 14:08:07 -0700

    cpu(def): define struct flag and interrupt

--- src/nes/cpu/cpu.c
+++ src/nes/cpu/cpu.c
@@ -5,14 +5,34 @@
 
 #include "cpu.h"
 
+#define MEMORY_SIZE 0xFFFF
+
+
 typedef struct cpu {
     uint8_t accumulator;
-    uint8_t x;
-    uint8_t y;
+    uint8_t register_x;
+    uint8_t register_y;
     uint16_t programCounter;
     uint8_t stackPointer;
-    uint8_t status;
+    uint8_t status;   
 } cpu_t;
+
+typedef enum {
+    NONE,
+    IRQ,
+    NMI
+} interrupt_t;
+
+typedef enum {
+    flag_carry,
+    flag_zero,
+    flag_interrupt,
+    flag_decimal,
+    flag_brk,
+    flag_overflow,
+    flag_negative
+} flag_t;
+
 
 typedef enum {
     NONE = 0,
@@ -23,6 +43,7 @@ typedef enum {
     ABSOLUTE,
     RELATIVE,
     INDIRECT,
+
     //Other addressing
     ZERO_PAGE_X,
     ZERO_PAGE_Y,
--- src/nes/cpu/cpu.h
+++ src/nes/cpu/cpu.h
@@ -1,7 +1,11 @@
 #ifndef CPU_H
 #define CPU_H
+#define MEMORY_SIZE 0xFFFF
 
 typedef struct cpu cpu_t;
+
 typedef enum addr_mode;
+typedef enum flag_t;
+typedef enum interrupt_t;
 
 #endif
